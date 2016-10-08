bc4812035d4fc2ec41446384d56fa6cae278869d
git <csinko@gmail.com>
2016-10-08 04:01:53 -0400

    fixed typo

--- web/app/routes.js
+++ web/app/routes.js
@@ -87,7 +87,7 @@ router.route('/refresh_token', function(req, res) {
   var refresh_token = req.query.refresh_token;
   var authOptions = {
     url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' client_secret).toString('base64')) },
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
     form: {
       grant_type: 'refresh_token',
       refresh_token: refresh_token
