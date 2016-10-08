c5d61d85411003063b4cac7b61008d65a3faee4b
git <csinko@gmail.com>
2016-10-08 04:10:36 -0400

    Added variables

--- web/app/routes.js
+++ web/app/routes.js
@@ -1,5 +1,8 @@
 var express  = require('express');
 module.exports = function(app) {
+  var client_id = 'eafbafd8462c416e9683f2cbecced544'; // Your client id
+  var client_secret = '6d0b5783b6954ddf8d156dcd34bbb035'; // Your secret
+  var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
   var router = express.Router();
   router.use(function(req, res, next) {
