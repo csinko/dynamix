7f5ca52cf9d4b0927f33a65e4da920f61b8bde86
git <csinko@gmail.com>
2016-10-08 04:12:47 -0400

    added body parser info

--- web/server.js
+++ web/server.js
@@ -4,12 +4,24 @@
 var express = require ('express');
 var request = require ('request');
 var querystring = require('querystring');
+var bodyParser     = require('body-parser');
 var cookieParser = require('cookie-parser');
 
 var app = express();
 
 app.use(express.static(__dirname + '/public'))
    .use(cookieParser());
+
+   app.use(bodyParser.json());
+
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+app.use(methodOverride('X-HTTP-Method-Override'));
 
 //routing -----------------------------
 require('./app/routes')(app)
