704b563c71a3dd8c929aa1daef01ee1671dd6a5b
EC2 Default User <ec2-user@ip-172-31-19-69.us-west-2.compute.internal>
2016-10-08 07:25:26 +0000

    removed servertest

--- Dynamix/server.js
+++
@@ -1,9 +0,0 @@
-/* Load HTTP library */
-var http = require("http");
-
-/* Create HTTP server to handle responses */
-http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Online.");
-	response.end();
-}).listen(8888);
