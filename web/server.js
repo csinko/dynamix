5184d8f12589ccc677088dd6d31b85c30fc771d4
git <csinko@gmail.com>
2016-10-08 04:13:38 -0400

    Added method override stuff

--- web/server.js
+++ web/server.js
@@ -5,6 +5,7 @@ var express = require ('express');
 var request = require ('request');
 var querystring = require('querystring');
 var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
 var cookieParser = require('cookie-parser');
 
 var app = express();
