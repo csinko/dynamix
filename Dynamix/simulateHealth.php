6f1fb046f307da485eb61418607051dfb3912e2d
agates4 <agate97@gmail.com>
2016-10-08 12:54:19 -0400

    Dynamix Stuff

---
+++ Dynamix/simulateHealth.php
@@ -0,0 +1,9 @@
+<?php include("global/header.html"); ?>
+
+<html>
+
+<div class="alert alert-success">
+  <strong>Success! <i class="fa fa-female" aria-hidden="true"></i></strong> Indicates a successful or positive action.
+</div>
+
+</html>
