d0643ebf7143c79191e5cd4b32000502f382ee07
Alexander-Ulery <alexanderulery@gmail.com>
2016-10-08 16:53:54 -0400

    Changed redirect uri (lines 12 - 14)

--- web/app/routes.js
+++ web/app/routes.js
@@ -9,9 +9,9 @@ module.exports = function(app) {
   var client_id = 'eafbafd8462c416e9683f2cbecced544'; // Your client id
   var client_secret = '6d0b5783b6954ddf8d156dcd34bbb035'; // Your secret
 
-  var redirect_uri = 'http://localhost:8888/api/spotify/callback';
+  //var redirect_uri = 'http://localhost:8888/api/spotify/callback';
 
-//  var redirect_uri = 'http://dynamix.tech:8888/api/spotify/callback'; // Your redirect uri
+  var redirect_uri = 'http://dynamix.tech:8888/api/spotify/callback'; // Your redirect uri
 
   var stateKey = 'spotify_auth_state';
 
@@ -121,7 +121,7 @@ module.exports = function(app) {
           };
 
           request.get(options, function(error, response, body) {
-            console.log(body);
+            console.log(body.tracks.items[0]);
           });
 
           res.redirect('/#' + querystring.stringify({
