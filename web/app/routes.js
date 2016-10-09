775d23a2c0ad07aaca5edff6a11d0049021352fd
git <csinko@gmail.com>
2016-10-09 02:11:40 -0400

    changed callback authorization

--- web/app/routes.js
+++ web/app/routes.js
@@ -204,17 +204,7 @@ module.exports = function(app) {
     return spotifyApi.getMe();
   })
   .then(function(data) {
-    // "Retrieved data for Faruk Sahin"
-    console.log('Retrieved data for ' + data.body['display_name']);
-
-    // "Email is farukemresahin@gmail.com"
-    console.log('Email is ' + data.body.email);
-
-    // "Image URL is http://media.giphy.com/media/Aab07O5PYOmQ/giphy.gif"
-    console.log('Image URL is ' + data.body.images[0].url);
-
-    // "This user has a premium account"
-    console.log('This user has a ' + data.body.product + ' account');
+    console.log(data);
   })
   .catch(function(err) {
     console.log('Something went wrong', err.message);
